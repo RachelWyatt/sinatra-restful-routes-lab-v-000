4f292013f55bdb82433a82ebf98e05a9e00fc3e9
Rachel Wyatt <rw126@evansville.edu>
2019-01-13 14:25:24 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,9 +15,9 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
-  get "/recipes/:id/edit" do 
+  get "/recipes/:id/edit" do
     @recipe = Recipe.find_by_id(params[:id])
-    erb :show
+    erb :edit
   end
 
 end
