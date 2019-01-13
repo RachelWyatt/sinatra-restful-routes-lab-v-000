872c9d74d0d7e59abaf0baef1463df4f2352575d
Rachel Wyatt <rw126@evansville.edu>
2019-01-13 14:09:32 +0000

    Automatically backed up by Learn

--- db/migrate/20190113140704_create_recipe.rb
+++ db/migrate/20190113140704_create_recipe.rb
@@ -2,7 +2,7 @@ class CreateRecipe < ActiveRecord::Migration
   def change
     create_table :recipes do |t|
       t.string :name
-      t.sring :ingredients
+      t.string :ingredients
       t.string :cook_time
     end
   end
